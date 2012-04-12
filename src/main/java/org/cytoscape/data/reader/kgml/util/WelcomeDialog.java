c3250fa02bc7b0559d3a608b489b3c6293645770
Keiichiro Ono <keiono@gmail.com>
2012-04-11 17:32:29 -0700

    Title image file had been added to the resource directory.

--- src/main/java/org/cytoscape/data/reader/kgml/util/WelcomeDialog.java
+++ src/main/java/org/cytoscape/data/reader/kgml/util/WelcomeDialog.java
@@ -6,8 +6,11 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
@@ -22,28 +25,31 @@ import org.cytoscape.data.reader.kgml.KGMLReaderPlugin;
 
 import cytoscape.Cytoscape;
 import cytoscape.CytoscapeInit;
+import cytoscape.logger.CyLogger;
 
 public final class WelcomeDialog extends JDialog {
 
 	private static final long serialVersionUID = -4848738704932153111L;
-
+	
 	private static final Font TITLE_FONT = new Font("SansSerif", Font.BOLD, 14);
 	private static final String DOC_PAGE_URL = "http://cytoscape.github.com/kgmlreader/";
-	
 
-	private static final String DESCRIPTION_TEXT = "<html><body>KGMLReader Plugin is a file reader for KEGG XML format (KGML).  " +
-			"To import KGML file, select <br /><ul><li>File-->Import-->Network</li></ul>"
-			+ "<p>For more information, please visit " +
-			"<a href='"+ DOC_PAGE_URL + "'>KGMLReader Plugin web site</a></p></body></html>";
+	private static final String DESCRIPTION_TEXT = "<html><body>" +
+			"<p>KGMLReader Plugin is a file reader for KEGG Pathway XML format (KGML).</p>"
+			+ "<p>- KGML files are available from <a href='http://www.kegg.jp/kegg/pathway.html'>KEGG web site</a>.</p>"
+			+ "<p>- To import KGML file, select <strong>File-->Import-->Network (Multiple File Types)</strong></p>"
+			+ "<p>For more information, please visit "
+			+ "<a href='"
+			+ DOC_PAGE_URL
+			+ "'>KGMLReader Plugin web site</a></p></body></html>";
 
 	private JLabel titleLabel;
-
 	private JEditorPane description;
-
 	private JButton closeButton;
-
 	private JCheckBox doNotShowAgainCheckBox;
 
+	
+	// Singleton
 	public static final WelcomeDialog DIALOG = new WelcomeDialog();
 
 	public static void showDialog() {
@@ -59,7 +65,9 @@ public final class WelcomeDialog extends JDialog {
 	private void initComponents() {
 		this.setTitle("Welcome to KGML Reader Plugin");
 
-		titleLabel = new JLabel("Welcome to KEGG Pathway XML File Reader Plugin!");
+		titleLabel = new JLabel();
+		final ImageIcon TITLE_IMAGE = new ImageIcon(this.getClass().getClassLoader().getResource("images/global_human_500px.png"));
+		titleLabel.setIcon(TITLE_IMAGE);
 		titleLabel.setFont(TITLE_FONT);
 		titleLabel.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
 		titleLabel.setHorizontalTextPosition(SwingConstants.CENTER);
@@ -75,8 +83,7 @@ public final class WelcomeDialog extends JDialog {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				CytoscapeInit.getProperties().setProperty(KGMLReaderPlugin.HIDE_WELCOME_SCREEN,
-						Boolean.toString(doNotShowAgainCheckBox.isSelected()));
+				saveProps();
 				setVisible(false);
 			}
 		});
@@ -87,7 +94,7 @@ public final class WelcomeDialog extends JDialog {
 		description = new JEditorPane();
 		description.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
 		description.setBackground(Color.white);
-		description.setPreferredSize(new Dimension(450, 200));
+		description.setPreferredSize(new Dimension(530, 200));
 		description.setEditable(false);
 		description.setContentType("text/html"); // javax.swing.text.html.HTMLEditorKit
 		description.putClientProperty(JEditorPane.HONOR_DISPLAY_PROPERTIES, Boolean.TRUE);
@@ -105,5 +112,22 @@ public final class WelcomeDialog extends JDialog {
 		this.add(description, BorderLayout.CENTER);
 		this.add(southPanel, BorderLayout.SOUTH);
 		pack();
+	}
+
+	private void saveProps() {
+		CytoscapeInit.getProperties().setProperty(KGMLReaderPlugin.HIDE_WELCOME_SCREEN,
+				Boolean.toString(doNotShowAgainCheckBox.isSelected()));
+		try {
+			File file = CytoscapeInit.getConfigFile("cytoscape.props");
+			FileOutputStream output = null;
+			try {
+				CytoscapeInit.getProperties().store(new FileOutputStream(file), "Cytoscape Property File");
+			} finally {
+				if (output != null)
+					output.close();
+			}
+		} catch (Exception ex) {
+			CyLogger.getLogger().error("Could not write cytoscape.props file!", ex);
+		}
 	}
 }
