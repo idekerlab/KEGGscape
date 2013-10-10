00409812b7c206e78d40bb3f158785c9432df472
Kozo Nishida <knishida@riken.jp>
2013-10-10 17:18:52 +0200

    removed old files once

--- src/main/java/org/cytoscape/data/reader/kgml/util/ColorParser.java
+++
@@ -1,9 +0,0 @@
-package org.cytoscape.data.reader.kgml.util;
-
-import java.awt.Color;
-
-public class ColorParser {
-	public static Color getColor(final String colorString) {
-		return null;
-	}
-}
