40f5351d7b0a3b3c25c72b41bd5ec2bf7a8ce93a
Kozo Nishida <knishida@riken.jp>
2013-11-18 07:25:34 +0900

    throw a network to KGMLMapper

--- src/main/java/org/cytoscape/keggscape/internal/read/kgml/KGMLMapper.java
+++ src/main/java/org/cytoscape/keggscape/internal/read/kgml/KGMLMapper.java
@@ -2,16 +2,19 @@ package org.cytoscape.keggscape.internal.read.kgml;
 
 import java.util.List;
 
+import org.cytoscape.model.CyNetwork;
 import org.cytoscape.keggscape.internal.generated.Entry;
 import org.cytoscape.keggscape.internal.generated.Pathway;
 
 public class KGMLMapper {
 	
 	private final Pathway pathway;
+	private final CyNetwork network;
 	private final String pathwayName;
 	
-	public KGMLMapper(final Pathway pathway) {
+	public KGMLMapper(final Pathway pathway, final CyNetwork network) {
 		this.pathway = pathway;
+		this.network = network;
 		this.pathwayName = pathway.getName();
 	}
 	
