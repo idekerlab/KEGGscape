41ffb8c838eea9e7179afda0624c7b361d92c675
Kozo Nishida <knishida@riken.jp>
2013-11-25 00:47:49 +0900

    add visualstyle builder for KGML

---
+++ src/main/java/org/cytoscape/keggscape/internal/KGMLVisualStyleBuilder.java
@@ -0,0 +1,63 @@
+package org.cytoscape.keggscape.internal;
+
+import java.awt.Paint;
+
+import org.cytoscape.view.presentation.property.BasicVisualLexicon;
+import org.cytoscape.view.vizmap.VisualMappingFunctionFactory;
+import org.cytoscape.view.vizmap.VisualStyle;
+import org.cytoscape.view.vizmap.VisualStyleFactory;
+import org.cytoscape.view.vizmap.mappings.PassthroughMapping;
+
+public class KGMLVisualStyleBuilder {
+	
+	// Default visual style name
+	public static final String DEF_VS_NAME = "KEGG Style";
+	
+	private final VisualStyleFactory vsFactory;
+	private final VisualMappingFunctionFactory passthroughMappingFactory;
+	
+	private static final String KEGG_NODE_X = "KEGG_NODE_X";
+	private static final String KEGG_NODE_Y = "KEGG_NODE_Y";
+	private static final String KEGG_NODE_WIDTH = "KEGG_NODE_WIDTH";
+	private static final String KEGG_NODE_HEIGHT = "KEGG_NODE_HEIGHT";
+	private static final String KEGG_NODE_LABEL = "KEGG_NODE_LABEL";
+	private static final String KEGG_NODE_LABEL_COLOR = "KEGG_NODE_LABEL_COLOR";
+	private static final String KEGG_NODE_FILL_COLOR = "KEGG_NODE_FILL_COLOR";
+
+	public KGMLVisualStyleBuilder(final VisualStyleFactory vsFactory,
+			final VisualMappingFunctionFactory passthroughMappingFactory) {
+		this.vsFactory = vsFactory;
+		this.passthroughMappingFactory = passthroughMappingFactory;
+	} 
+	
+	public VisualStyle getVisualStyle() {
+		
+		final VisualStyle defStyle = vsFactory.createVisualStyle(DEF_VS_NAME);
+		
+		final PassthroughMapping<String, Double> nodexPassthrough = (PassthroughMapping<String, Double>) passthroughMappingFactory
+				.createVisualMappingFunction(KEGG_NODE_X, String.class, BasicVisualLexicon.NODE_X_LOCATION);
+		final PassthroughMapping<String, Double> nodeyPassthrough = (PassthroughMapping<String, Double>) passthroughMappingFactory
+				.createVisualMappingFunction(KEGG_NODE_Y, String.class, BasicVisualLexicon.NODE_Y_LOCATION);
+		final PassthroughMapping<String, Double> nodewidthPassthrough = (PassthroughMapping<String, Double>) passthroughMappingFactory
+				.createVisualMappingFunction(KEGG_NODE_WIDTH, String.class, BasicVisualLexicon.NODE_WIDTH);
+		final PassthroughMapping<String, Double> nodeheightPassthrough = (PassthroughMapping<String, Double>) passthroughMappingFactory
+				.createVisualMappingFunction(KEGG_NODE_HEIGHT, String.class, BasicVisualLexicon.NODE_HEIGHT);
+		final PassthroughMapping<String, String> nodelabelPassthrough = (PassthroughMapping<String, String>) passthroughMappingFactory
+				.createVisualMappingFunction(KEGG_NODE_LABEL, String.class, BasicVisualLexicon.NODE_LABEL);
+		final PassthroughMapping<String, Paint> nodelabelcolorPassthrough = (PassthroughMapping<String, Paint>) passthroughMappingFactory
+				.createVisualMappingFunction(KEGG_NODE_LABEL_COLOR, String.class, BasicVisualLexicon.NODE_LABEL_COLOR);
+		final PassthroughMapping<String, Paint> nodefillcolorPassthrough = (PassthroughMapping<String, Paint>) passthroughMappingFactory
+				.createVisualMappingFunction(KEGG_NODE_FILL_COLOR, String.class, BasicVisualLexicon.NODE_FILL_COLOR);
+				
+		defStyle.addVisualMappingFunction(nodexPassthrough);
+		defStyle.addVisualMappingFunction(nodeyPassthrough);
+		defStyle.addVisualMappingFunction(nodewidthPassthrough);
+		defStyle.addVisualMappingFunction(nodeheightPassthrough);
+		defStyle.addVisualMappingFunction(nodelabelPassthrough);
+		defStyle.addVisualMappingFunction(nodelabelcolorPassthrough);
+		defStyle.addVisualMappingFunction(nodefillcolorPassthrough);
+		
+		return defStyle;
+	}
+
+}
