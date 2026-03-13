6e51a228478a336b04bb8555d2a320d3a0ffebb0
Kozo Nishida <kozo.nishida@gmail.com>
2026-03-14 03:39:26 +0900

    Remove unused Swagger import
    Remove the unused io.swagger.annotations.ApiModelProperty import from KeggscapeDocumentation.java to clean up the code and avoid compiler/IDE warnings.

--- src/main/java/org/cytoscape/keggscape/internal/KeggscapeDocumentation.java
+++ src/main/java/org/cytoscape/keggscape/internal/KeggscapeDocumentation.java
@@ -1,7 +1,5 @@
 package org.cytoscape.keggscape.internal;
 
-import io.swagger.annotations.ApiModelProperty;
-
 public class KeggscapeDocumentation {
 	public static final String GENERIC_SWAGGER_NOTES = "KEGGscape will import the KEGG pathway (specified by {pathid}) into Cytoscape." + '\n' + '\n' 
 	+ "For example, /keggscape/v1/eco00010 imports http://www.genome.jp/kegg-bin/show_pathway?eco00010 into Cytoscape." + '\n' + '\n'
